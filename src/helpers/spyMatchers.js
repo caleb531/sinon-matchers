09b6e55cfaf265933e63a41a618463f38f1add9a
Caleb Evans <caleb@calebevans.me>
2022-12-20 13:12:03 -0800

    Fix error with `expect` not being require'able

--- src/helpers/spyMatchers.js
+++ src/helpers/spyMatchers.js
@@ -3,8 +3,13 @@ import path from 'path';
 /**
  * A hack to get around the Package entry points used by jest
  */
-const spyMatchers = require(`${path.dirname(
+let spyMatchers;
+try {
+  spyMatchers = require(`${path.dirname(
   require.resolve('expect')
 )}/spyMatchers.js`).default;
+} catch (error) {
+  spyMatchers = {};
+}
 
 export default spyMatchers;
--- src/helpers/toThrowMatchers.js
+++ src/helpers/toThrowMatchers.js
@@ -3,8 +3,13 @@ import path from 'path';
 /**
  * A hack to get around the Package entry points used by jest
  */
-const toThrowMatchers = require(`${path.dirname(
-  require.resolve('expect')
-)}/toThrowMatchers.js`).default;
+let toThrowMatchers;
+try {
+  toThrowMatchers = require(`${path.dirname(
+    require.resolve('expect')
+  )}/toThrowMatchers.js`).default;
+} catch (error) {
+  toThrowMatchers = {};
+}
 
 export default toThrowMatchers;
